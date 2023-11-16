ad48cf0d6b710aa5938bb5d150ee95ca08754a80
Gavin Feriancek <gferiancek@gmail.com>
2023-11-15 23:56:17 -0500

    Part 1: servers.test.js tests

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,40 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe('servers.js test', function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+  describe('submitServerInfo() tests', function () {
+    it('should add a new server to allServers', function () {
+      submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+      expect(Object.keys(allServers).length).toEqual(1);
+      expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
+
+    it('should not add a new server if serverName is empty', function () {
+      serverNameInput.value = '';
+      submitServerInfo();
+
+      expect(Object.keys(allServers).length).toEqual(0);
+    });
   });
 
-  afterEach(function() {
+  describe('updateServerTable() tests', function () {
+    it('should append new tr serverTable', function () {
+      submitServerInfo();
+
+      let tdList = document.querySelectorAll('#server1 td');
+      expect(tdList.length).toEqual(2);
+      expect(tdList[0].innerText).toEqual('Alice');
+      expect(tdList[1].innerText).toEqual('$0.00');
+    });
+  });
+
+  afterEach(function () {
     // teardown logic
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = '';
   });
 });
